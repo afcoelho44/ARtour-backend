75211ca6c1fb67d6426d949186200393ba868bf1
felip <felipevendrami@hotmail.com>
2023-11-02 22:29:23 -0300

    Cors fix

---
+++ src/main/java/com/br/artour/Config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.br.artour.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Permitimos que as requisições venham de qualquer origem (sem segurança), devido ao erro de CORS
+        registry.addMapping("/**");
+    }
+}
